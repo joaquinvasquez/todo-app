6e06fa09440c14e05d51af87d89a3daaa17d3336
Joaquin Vasquez <joaquinvasquez20@gmail.com>
2024-01-23 23:41:50 -0300

    [back] db deploy fixes

--- backend/models/mysql/db-config.js
+++ backend/models/mysql/db-config.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise'
-const dbConfig = {
+const DEFAULT_CONFIG = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 }
-
+const dbConfig = process.env.DATABASE_URL ?? DEFAULT_CONFIG
 export const dbConnection = await mysql.createConnection(dbConfig)
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import 'dotenv/config'
 import { todoRouter } from './router/todos.js'
 import { corsMiddleware, corsPreflightMiddleware } from './middlewares/cors.js'
 import { errorHandler } from './middlewares/errorHandler.js'
